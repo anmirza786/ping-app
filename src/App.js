49fd8cfe799ea3c3c65588026a37414b641c0290
anmirza786 <anmirza360@gmail.com>
2023-02-08 16:04:18 +0500

    used axios instead of fetch, removed all the consoles reset the code structure and removed all the non used states

--- src/App.js
+++ src/App.js
@@ -1,102 +1,90 @@
-import './App.css';
-import Table from './Table';
-import { useState } from 'react'
-import { regions } from './regions';
+import "./App.css";
+import Table from "./Table";
+import { useEffect, useState } from "react";
+import { regions } from "./regions";
+import axios from "axios";
 
-
-const ping = async (url,data, timeout = 6000) => {
-  return await new Promise((resolve, reject) => {
-    let start = new Date().getTime();
-    try {
-      fetch(url)
-        .then(() => {
-          const currentTime = new Date().getTime()
-          const newdata = {
-            ...data,
-            pingtime:[...data.pingtime,currentTime-start]
-          }
-          
-          resolve(newdata)
-        })
-        .catch(() => {
-          const newdata = {
-            ...data,
-            pingtime:[0]
-          }
-          resolve(newdata)
-        });
-      setTimeout(() => {
-        resolve(0);
-      }, timeout);
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
+// const ping = async (url, data, timeout = 6000) => {
+//   return await new Promise((resolve, reject) => {
+//     let start = new Date().getTime();
+//     try {
+//       fetch(url)
+//         .then(() => {
+//           const currentTime = new Date().getTime();
+//           const newdata = {
+//             ...data,
+//             pingtime: [...data.pingtime, currentTime - start],
+//           };
+//           resolve(newdata);
+//         })
+//         .catch(() => {
+//           const newdata = {
+//             ...data,
+//             pingtime: [0],
+//           };
+//           resolve(newdata);
+//         });
+//       setTimeout(() => {
+//         resolve(0);
+//       }, timeout);
+//     } catch (e) {
+//       reject(e);
+//     }
+//   });
+// };
 
 function App() {
-  const [newdataping,setNewDataPing] = useState(null)
-  const [testobj,setTestObj] = useState({})
-  const [pingedData,setPingedData] = useState(null)
-  let newarray = []
-  function pingFunction (array){
-    newarray = []
-    array.forEach(r=>{
-      let newdata
-      if(testobj){
-        newdata= {
-          name:r.name,
-          geography: r.geography,
-          regionName: r.regionName,
-          pingtime: r.pingtime
-        }
-      }
-      else
-      {newdata= {
-        name:r.name,
+  const [newDataPing, setNewDataPing] = useState([]);
+
+  const pingFunction = () => {
+    setNewDataPing([]);
+    regions.forEach(async (r) => {
+      let newdata = {
+        name: r.name,
         geography: r.geography,
         regionName: r.regionName,
-        pingtime: []
-      }}
-      ping(`https://dynamodb.${r.regionName}.amazonaws.com/ping?x=35gg91n9v9i00`,newdata)
-      .then(res=>
-        {
-          setTestObj(res)
-          newarray.push(res)
-        }
+        availabilityZoneCount: r.availabilityZoneCount,
+        pingtime: [],
+      };
+      // ping(
+      //   `https://dynamodb.${r.regionName}.amazonaws.com/ping?x=35gg91n9v9i00`,
+      //   newdata
+      // ).then((res) => {
+      //   setNewDataPing((prevState) => [...prevState, res]);
+      // });
+      let start = new Date().getTime();
+      await axios
+        .get(
+          `https://dynamodb.${r.regionName}.amazonaws.com/ping?x=35gg91n9v9i00`
         )
-      }) 
-      setNewDataPing(newarray)
-      renderTable()
-  }
-  function handelping (){
-    if (pingedData){
-      pingFunction(pingedData)
-    }
-    else{
-      pingFunction(regions)
-    }
-  }
-  function renderTable(){
-    
-    if (newdataping)
-      return <Table data = {newdataping} pingedData = { setPingedData }/>
-    else
-      return <h1>Ping Please</h1>
-  }
+        .then((res) => {})
+        .catch((err) => {
+          const currentTime = new Date().getTime();
+          newdata = {
+            ...newdata,
+            pingtime: [...newdata.pingtime, currentTime - start],
+          };
+          setNewDataPing((prevState) => [...prevState, newdata]);
+        });
+    });
+  };
+  useEffect(() => {
+    pingFunction();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        
-        <button
-          className="App-link"
-          onClick={handelping}
-        >
+        <button className="App-link" onClick={pingFunction}>
           Ping
         </button>
       </header>
       <div className="data">
-        {newdataping && renderTable()}
+        {newDataPing.length ? (
+          <Table data={newDataPing} />
+        ) : (
+          <h1>Ping Please</h1>
+        )}
       </div>
     </div>
   );
